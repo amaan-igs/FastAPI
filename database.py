a2acb408d8dce522457eaedad2260f33a96d8449
Amaan Ul Haq Siddiqui <amaanulhaq.siddiqui@sudoconsultants.com>
2025-02-03 12:25:22 +0500

    feat: Configure database settings on AWS

--- database.py
+++ database.py
@@ -1,11 +1,24 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+tmpPostgres = urlparse(DATABASE_URL)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+    }
+}
 
 def get_db():
     conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
